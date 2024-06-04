4800ab8c056bfa18d35e7b32ee8b976533c5be82
RatingTax78753 <ruanvr2006@gmail.com>
2024-06-04 10:06:28 -0300

    atualização js basica

--- static/js/preview-imagem.js
+++ static/js/preview-imagem.js
@@ -5,8 +5,13 @@ function previewImage(event) {
         var imagemPreviewPerfil = document.getElementById('imagem-preview-perfil');
         var imagemPreviewPost = document.getElementById('imagem-preview-post');
         
-        if (imagemPreviewPerfil) imagemPreviewPerfil.src = reader.result;
-        if (imagemPreviewPost) imagemPreviewPost.src = reader.result;
+        if (imagemPreviewPerfil) {
+            imagemPreviewPerfil.src = reader.result;
+        }
+        
+        if (imagemPreviewPost) {
+            imagemPreviewPost.src = reader.result;
+        }
     };
     reader.readAsDataURL(input.files[0]);
 }
